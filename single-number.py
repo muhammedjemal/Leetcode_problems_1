7de9a5478528eefacb6533ce833b7fd02523683e
jems jr <108370284+muhammedjemal@users.noreply.github.com>
2023-02-01 11:41:38 +0300

    Create single-number.py

---
+++ single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        number = nums[0]
+
+        for i in range(1, len(nums)):
+            number ^= nums[i]
+
+        return number
